a8d0bd48a00f21ff2c216faff8e2526916428ffa
norbert sackey <norbert.sackey@ashesi.edu.gh>
2014-09-10 22:51:00 +0100

    just added a few more comments to my code

--- Tester.java
+++ Tester.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+//@Norbert Sackey CS2016
 
-
+// this is a sort of multi-purpose class which does a number of operations depending on parameters given it 
 public class Tester{
   
+  //the main 
   public static void main(String[] args){
    
   int count;  
@@ -14,6 +16,7 @@ Scanner input  = new Scanner(System.in);
 
  entry = input.next();
  
+ //if the parameter is table followed by a number  "n"
  if(entry.charAt(0)=='t' && entry.charAt(1)=='a' &&entry.charAt(2)=='b' && entry.charAt(3)=='l' && entry.charAt(4)=='e'){
  number = input.nextInt();
  
@@ -22,6 +25,7 @@ Scanner input  = new Scanner(System.in);
  }
  }
  
+ // if the parameter is fact followed by a number "n"
  else if(entry.charAt(0)=='f' && entry.charAt(1)=='a'&& entry.charAt(2)=='c' && entry.charAt(3)=='t'){
  number = input.nextInt();
 if (number < 0){
