bc0874fc3469b1b57fe355adceca415d1a550a1a
norbert sackey <norbert.sackey@ashesi.edu.gh>
2014-09-10 19:11:40 +0100

    added Arc.java

---
+++ Arc.java
@@ -0,0 +1,23 @@
+public class Arc{
+	
+private static int[] inbox ; 
+
+public static void FillInbox(){
+	int k = 0;
+	for(i=0; i<10 ; i++){
+	k = i ; 
+	k++;
+}
+}
+
+
+public static void displayInbox(){
+	for(i=0; i<10 ; i++)
+	System.out.println(i);
+	}
+
+public static void main(String[]args){
+	
+	FillInbox();
+	displayInbox();
+}
