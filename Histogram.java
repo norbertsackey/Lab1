14dc269cf7ba0e71b6e92b9160bf6ca3a284ab3f
norbert sackey <norbert.sackey@ashesi.edu.gh>
2014-09-11 15:11:36 +0100

    added a little code

--- Histogram.java
+++ Histogram.java
@@ -1,8 +1,17 @@
 public class Histogram{
 
-  public static int[ ] lengths = new int[ ]  {4,20,7,18,35,6,14,39,21,5};
+  public static int[ ] num = new int[ ]  {4,20,7,18,35,6,14,39,21,5};
+  public static symbol = '$'
 
-public static void 
+  public static void main(String[] args){
+  
+    for(int i = 0 ; i < num.length ; i++){
+      for(i= 0  ; i 
+      
+    }
+  
+  
+  }
 
 
 
