06e5d675e9177451a40a924e29f96c3526038877
norbert sackey <norbert.sackey@ashesi.edu.gh>
2014-09-11 15:43:16 +0100

    updated the Histogram class to print axes

--- Histogram.java
+++ Histogram.java
@@ -1,19 +1,26 @@
 public class Histogram{
 
   public static int[ ] num = new int[ ]  {4,20,7,18,35,6,14,39,21,5};
-  public static symbol = '$'
+  public static char symbol = '$' ;
 
   public static void main(String[] args){
   
     for(int i = 0 ; i < num.length ; i++){
-      for(i= 0  ; i 
-      
+      System.out.print(" | ");
+      for(int n= 0  ; n < num[i] ; n++){
+      System.out.print(symbol);
+      }
+      System.out.println();
     }
-  
+    
+ 
+  System.out.println("0");
+   int k = 1 ;
+   while(k != 40){
+   System.out.print(" ");
+   k++;
+   }
+  System.out.print(k);
   
   }
-
-
-
-
 }
