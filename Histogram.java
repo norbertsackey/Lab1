04e66f2b031db720ab71bcd07282016bbbbdb02d
norbert sackey <norbert.sackey@ashesi.edu.gh>
2014-09-11 15:53:17 +0100

    finalised the Histogram class

--- Histogram.java
+++ Histogram.java
@@ -1,10 +1,11 @@
 public class Histogram{
 
   public static int[ ] num = new int[ ]  {4,20,7,18,35,6,14,39,21,5};
-  public static char symbol = '$' ;
+  public static char symbol = '*' ;
 
   public static void main(String[] args){
   
+    System.out.println("Y");
     for(int i = 0 ; i < num.length ; i++){
       System.out.print(" | ");
       for(int n= 0  ; n < num[i] ; n++){
@@ -12,15 +13,17 @@ public class Histogram{
       }
       System.out.println();
     }
-    
- 
-  System.out.println("0");
+  
+
+   
+  System.out.print(" ");
    int k = 1 ;
    while(k != 40){
-   System.out.print(" ");
+   System.out.print("*");
    k++;
    }
-  System.out.print(k);
+  System.out.print("X");
+  System.out.println();
   
   }
 }
